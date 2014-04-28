d10c84bd1e6127df92fffe74ef88ef69206be82d
mikex32 <mike.mikex@gmail.com>
2014-04-28 18:18:15 +0200

    USB communication included

--- sw/curve-tracer-gui/Forms/frmMain.Designer.cs
+++ sw/curve-tracer-gui/Forms/frmMain.Designer.cs
@@ -29,11 +29,169 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.port = new System.IO.Ports.SerialPort(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // port
+            // 
+            this.port.BaudRate = 115200;
+            this.port.PortName = "COM10";
+            this.port.ReadBufferSize = 16384;
+            this.port.ReadTimeout = 1000;
+            this.port.ReceivedBytesThreshold = 8;
+            this.port.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.port_DataReceived);
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.Crossing = -1.7976931348623157E+308D;
+            chartArea1.AxisX.Interval = 0.1D;
+            chartArea1.AxisX.MajorGrid.LineColor = System.Drawing.Color.Silver;
+            chartArea1.AxisX.Maximum = 3D;
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.AxisX.MinorGrid.LineColor = System.Drawing.Color.Silver;
+            chartArea1.AxisY.Interval = 0.1D;
+            chartArea1.AxisY.MajorGrid.LineColor = System.Drawing.Color.Silver;
+            chartArea1.AxisY.Maximum = 3D;
+            chartArea1.AxisY.Minimum = 0D;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.None;
+            this.chart1.PaletteCustomColors = new System.Drawing.Color[] {
+        System.Drawing.Color.Black};
+            this.chart1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.IsVisibleInLegend = false;
+            series1.Legend = "Legend1";
+            series1.Name = "va_char";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(742, 420);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(623, 457);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.DecimalPlaces = 1;
+            this.numericUpDown2.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.numericUpDown2.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDown2.Location = new System.Drawing.Point(316, 457);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(44, 20);
+            this.numericUpDown2.TabIndex = 3;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(834, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(760, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Max voltage:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(875, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "V";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(224, 459);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Set max voltage:";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 516);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "frmMain";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.IO.Ports.SerialPort port;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
-
--- sw/curve-tracer-gui/Forms/frmMain.cs
+++ sw/curve-tracer-gui/Forms/frmMain.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CurveTracerGui
@@ -15,5 +17,84 @@ namespace CurveTracerGui
         {
             InitializeComponent();
         }
+
+        UInt16 oldid = 0;
+        double[] voltage = new double[4096];
+        double[] current = new double[4096];
+        double[] m_current = new double[4096];
+        decimal set_max_voltage = 0;
+        string max_voltage;
+        int send_max = 0;
+
+
+        private UInt16 ReadU16(System.IO.Stream stm)
+        {
+            byte[] buffer = new byte[2];
+            stm.Read(buffer, 0, 2);
+            return (UInt16)(buffer[0] + buffer[1] * 256);
+        }
+
+        private void port_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            while (port.BytesToRead > 8)
+            {
+                UInt16 id = ReadU16(port.BaseStream);
+
+                if ((oldid + 1 != id) && (id != 1) && (oldid != 0xFFF))
+                {
+                    Trace.WriteLine("LOST");
+                    oldid = 0;
+                    continue;
+                }
+
+                oldid = id;
+                m_current[id] = (ReadU16(port.BaseStream));
+                
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            max_voltage = numericUpDown2.Value.ToString();
+            label1.Text = max_voltage;
+
+            for (int i = 0; i < 4096; i++)
+            {
+                voltage[i] = (i / 1530) + 0.17;
+            }
+            for (int i = 0; i < 2048; i++)
+            {
+                current[i] = i/1000;
+            }
+            for (int i = 2048; i < 4096; i++)
+            {
+                current[i] = (4096-i)/1000;
+            }
+            chart1.Series[0].Points.Clear();
+
+            for (int i = 1; i < 4097; i++)
+            {
+                chart1.Series[0].Points.AddXY(voltage[i-1], current[i-1]);
+            }
+           
+        }
+        
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            port.Open();
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            set_max_voltage = numericUpDown2.Value;
+            send_max = (int)(((float)set_max_voltage-0.017)/ 0.000654);
+            max_voltage = numericUpDown2.Value.ToString();
+            label1.Text = max_voltage;
+
+
+        }
+
+
+        
     }
 }
